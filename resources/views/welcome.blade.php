47b08c53b33ad195eb3db06f23ae25f982920428
Petr Reshetin <preshetin@gmail.com>
2017-02-28 11:34:44 +0000

    Procfile for Heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Quiz Overflow
                 </div>
 
                 <div class="links">
